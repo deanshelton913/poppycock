a82d691b2c50e6a225ef4c4ba0c082e0f54ae208
Matthew  Moya <sirmoyap5@gmail.com>
2015-12-31 19:59:51 -0800

    whatever

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PoppyCock' });
 });
 
-router.get('/votes', function(req, res, next) {
-  res.render('votes', { title: 'PoppyCock' });
+router.get('/votes/new', function(req, res, next) {
+  res.render('votes/new', { title: 'PoppyCock' });
 });
 
 router.get('/definitions', function(req, res, next) {
-  res.render('definitions', { title: 'PoppyCock' });
+  res.render('definitions/index', { title: 'PoppyCock' });
+});
+
+router.get('/definitions/new', function(req, res, next) {
+  res.render('definitions/new', { title: 'PoppyCock' });
 });
 
 router.get('/how-to', function(req, res, next) {
